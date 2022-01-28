dbb7cb75f0aba425a4b9c4ca8b7fb1a4f548e904
gabrielfv-santos <gabriel.santos@equals.com.br>
2022-01-27 21:46:42 -0300

    adiiconando google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { filter } from 'rxjs/operators';
+import { NavigationEnd, Router } from '@angular/router';
 
+
+declare let gtag: Function;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pokeshop';
+
+  id = Math.floor(Math.random() * 10);
+
+
+  constructor(private router: Router) {
+    localStorage.setItem('userId', this.id.toString())
+  }
+
+  ngOnInit() {
+      this.setUpAnalytics();
+  }
+
+  setUpAnalytics() {
+      this.router.events.pipe(filter(event => event instanceof NavigationEnd))
+          .subscribe((event) => {
+            console.log(event)
+            if (event instanceof NavigationEnd){
+                gtag('config', 'G-G3K9TW6MYG', {
+                  page_path: event.urlAfterRedirects,
+                  user_id: localStorage.getItem('userId')
+                }
+              );
+            }
+          });
+    }
 }
